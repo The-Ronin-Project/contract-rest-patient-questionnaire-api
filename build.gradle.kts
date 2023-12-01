a23127f4c5589773ddefbc0fab3d938e3f0ecf86
Ross M. Lodge <ross@projectronin.com>
2023-12-01 13:01:41 -0800

    Update plugin version and use openapi-generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,7 @@ group = "com.projectronin.rest.contract"
 plugins {
     alias(roningradle.plugins.openapi.contract)
 }
+
+restContractSupport {
+    generatorType.set(com.projectronin.rest.contract.GeneratorType.OPENAPI_GENERATOR)
+}
