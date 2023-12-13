717fb041954caf037f76b1db095f8ff148bcc9c3
jeffbennettprojectronin <122327690+jeffbennettprojectronin@users.noreply.github.com>
2023-12-13 14:29:29 -0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,3 +7,11 @@ plugins {
 restContractSupport {
     generatorType.set(com.projectronin.rest.contract.GeneratorType.OPENAPI_GENERATOR)
 }
+
+tasks.generateOpenApiCodeOpenApiGenerator {
+    configOptions.set(
+        mapOf(
+            "useTags" to "true"
+        )
+    )
+}
